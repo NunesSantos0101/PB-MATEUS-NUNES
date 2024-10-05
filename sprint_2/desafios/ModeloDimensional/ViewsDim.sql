2a4f92768997fb6bc7e626e8e9522f5814413049
NunesSantos0101 <mateussantosdev2002@gmail.com>
2024-10-05 18:20:46 -0300

    enviando desafios e evidencias

---
+++ sprint_2/desafios/ModeloDimensional/ViewsDim.sql
@@ -0,0 +1,58 @@
+CREATE VIEW dimCliente AS
+SELECT
+    idCliente as cliente,
+    nomeCliente as nome,
+    cidadeCliente as cidade,
+    estadoCliente as estado,
+    paisCliente as pais
+FROM
+    Clientes;
+
+CREATE VIEW dimVendedor AS
+SELECT
+	idVendedor as vendedor,
+	nomeVendedor as nome,
+	sexoVendedor as sexo,
+	estadoVendedor as estado
+FROM 
+	Vendedores;
+	
+CREATE VIEW dimCarro AS
+SELECT
+    idCarro as carro,
+     kmCarro as km,
+    classiCarro as classi,
+    marcaCarro as marca,
+    modeloCarro as modelo,
+    anoCarro as ano,
+    idCombustivel
+FROM
+    Carros
+
+CREATE VIEW dimCombustivel AS 
+SELECT 	
+	idCombustivel as combustivel,
+	tipoCombustivel
+
+FROM Combustivel;
+
+ CREATE VIEW dimEntrega AS
+ SELECT 
+ 	idLocacao,
+ 	dataEntrega,
+    horaEntrega
+FROM Locacao 	
+
+
+CREATE VIEW dimLocacao AS
+SELECT
+    idLocacao,
+    dataLocacao,
+    horaLocacao,
+    qtdDiaria,
+    vlrDiaria,
+    idCliente,
+    idCarro,
+    idVendedor
+FROM
+    Locacao;
